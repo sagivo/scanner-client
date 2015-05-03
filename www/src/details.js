c26288dc7942d98eb047df713b1dcb6153f62a06
Sagiv <sagiv4@gmail.com>
2015-05-03 04:05:49 -0700

    Pages

--- www/src/details.js
+++ www/src/details.js
@@ -1,6 +1,6 @@
 var template = document.querySelector('template[is=auto-binding]'); 
 template.data = [
-	{ text: 'hello, my name is all i love', url: 'http://www.rockdesign.com/prod_images/black-duplex-business-cards2_4240.jpg' }, 
+	{ text: 'Atalantic.net - 5/3/15 @TC Disrupt) Fast SSd Cloud Hosting Ready in 30 Seconds $25 Free Credit When you use code TECHCRUNCH25 No Contract RESTFUL API Pay as you go Windows, Linux, FreeBSD One-Click Apps Global Data Center', url: 'http://i.imgur.com/3iDgfLD.jpg' }, 
 	{ text: 'lorem ipsum delom', url: 'http://www.creative82.co.uk/perch/resources/businesscardsnew-.jpg'}, 
 	{ text: 'because i got heigh', url: 'http://asset.zcache.com/assets/graphics/z4/hubSubsites/Create_Section/Wide/Business_Cards_Various.jpg' }, 
 	{ text: 'Roller because i got hi.', url: 'http://businesscardjournal.com/wp-content/uploads/Modern-Tricolor-Business-Card-Template-For-Corporate-Professional-0014.jpg' }
@@ -8,6 +8,6 @@ template.data = [
 
 template.filter = function(d, filter){
 	return d.filter(function(el){
-		return (el.text.indexOf(filter) > -1);
+		return (el.text.toLowerCase().indexOf(filter.toLowerCase()) > -1);
 	});
 }
